6da06992a8311156d8e02234b5d15eba9000ec1b
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-14 07:12:33 +0300

    add attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """An square class with a private object attribute"""
+    def __init__(self, size):
+        self.__size = size
