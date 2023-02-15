f8ce54cca534e07a8059a45a6a96b184d61427a0
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-15 13:56:36 +0300

    converts roman numerals to interger

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,41 +1,11 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    try:
-        roman_string = roman_string.upper()
-    except AttributeError:
+    if not roman_string or type(roman_string) != str:
         return 0
-    else:
-        num = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-    res = 0
-    skip = False
-
-    end_index = len(roman_string) - 1
-
-    for i in range(end_index, -1, -1):
-        if skip:
-            skip = False
-            continue
-
-        if roman_string[i] in num.keys():
-            curr = num[roman_string[i]]
-            next = num[roman_string[i - 1]]
-
-            if i == 0:
-                next = 1000000
-
-            if curr > next:
-                res += (curr - next)
-                skip = True
-            else:
-                res += curr
-        else:
-            return 0
-    return res
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total
