86a5c74def6338dcdc2c55b9be78d164e810631b
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-15 16:29:15 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
+    from hidden_4 import *
+    ls = dir()
+    for name in ls:
         if name[:2] != "__":
             print(name)
