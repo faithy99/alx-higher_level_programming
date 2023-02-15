834a566a5159b027e5b469c060d368efa79c8165
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-15 11:57:54 +0300

    sorted keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for key in keys:
+        print(f"{key}: {a_dictionary[key]}")
