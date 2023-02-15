c9b393883913716102adf062d751958605eb2006
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-15 11:32:55 +0300

    s-simple matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    new_matrix = []
+
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x**2, i)))
+
     return new_matrix
