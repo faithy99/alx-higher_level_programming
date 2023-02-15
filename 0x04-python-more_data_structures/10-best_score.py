3b23c9fad22bb89a947d835f87a25173b81ed70a
FaithAnn Njoroge <faithannenjoroge@gmail.com>
2023-02-15 13:53:43 +0300

    key of the biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    if len(a_dictionary.values()) == 0:
-        return None
-
-    max_value = max(a_dictionary.values())
-
-    for key, value in a_dictionary.items():
-        if value == max_value:
-            return 
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
